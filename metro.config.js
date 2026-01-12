a5b6775aebc32b0bdf1e7adb67e0e864f7b82a0a
Mindul <pencake33@naver.com>
2026-01-12 18:21:45 +0900

    제발... metro 설정 변경

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.nodeModulesPaths = [path.resolve(__dirname, "node_modules")];
+config.resolver.disableHierarchicalLookup = true;
 
 module.exports = withNativeWind(config, { input: "./global.css" });
