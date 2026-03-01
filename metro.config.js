6ce19050764222e19d75eb82135d1e09a4907fd6
Mindul <pencake33@naver.com>
2026-03-01 19:14:56 +0900

    sentry 일단 제거

--- metro.config.js
+++
@@ -1,6 +0,0 @@
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
-const config = getSentryExpoConfig(__dirname);
-// pnpm 호이스팅 모드니까 복잡한 설정 다 빼고 이것만!
-module.exports = config;
